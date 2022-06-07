ef02e5b46a97bc024dbd8d5faab9a31f920c6a3b
Emanuel Martinez <yinproducciones@gmail.com>
2022-06-06 22:16:42 -0300

    creando funciones y ciclos primer desafio entregable

--- main.js
+++ main.js
@@ -1,18 +1,24 @@
-let pedirNumero = Number (prompt ("Ingresa un numero por favor"));
 
-for (let i = 1; i <= 5; i++) {
-    let resultado = pedirNumero + i;
-    alert(pedirNumero + " + " + i + " = " + resultado)
-};
+let respuesta = 'si'
 
-let pedirTexto = (prompt ("Por favor ingresa un texto aqui"));
+while(respuesta.toLowerCase() === 'si') {
+    const servicio = prompt("Ingrese el servicio que desea suscribirse: Netflix, Amazon prime, HBO");
+    let tiempo = Number(prompt("Ingrese numero de meses que desea estar suscrito: 1 mes, 3 meses, 12 meses"));
+    let pantallas = Number(prompt("Ingrese numero de pantallas que va a utilizar para el servicio: 1, 2, 4"));
+    let personas = Number(prompt("Ingrese numero de personas que van a utilizar el servicio: 1, 2, 4"));
 
-while (pedirTexto != "ESC" ){
-    alert("Has ingresado "+ pedirTexto);
-    pedirTexto = prompt("Ingresa otro texto para continuar...psst! ESC ;)");
-};
+    const costoServicio = calcularCosto(tiempo, pantallas, personas);
 
-/*let numero = Number (prompt ("Escribi un numero "));
-do{
-    numero = 
-}*/
+    if (costoServicio >= 4000) {
+        alert("Felicidades! adquiriste "+ servicio + " premium " + "por un precio de " + costoServicio)
+    } else {
+        alert("Felicidades! adquiriste " + servicio + " basico " + " por un precio de " + costoServicio)
+    }
+
+    respuesta = prompt('¿Deseas adquirir otro servicio?');
+}
+
+function calcularCosto(tiempo, pantallas, personas) {
+    let costoServicio = ((tiempo, pantallas, personas) * 1500)
+    return costoServicio ;
+}
